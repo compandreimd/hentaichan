162192c3abe0a240c9f42455b38f22c4e08a8aaa
Andrei <compandreimd@gmail.com>
2025-03-27 09:10:06 +0200

    Reset

--- www/js/index.js
+++ www/js/index.js
@@ -1,54 +1,29 @@
-// This JavaScript files runs on Cordova UI
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-function ctrl_log() {
-  return document.getElementById('logs');
-}
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
 
-function log(x) {
-  ctrl_log().innerHTML += x + "<BR/>";
-  ctrl_log().parentNode.scrollTop = ctrl_log().clientHeight;
-}
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
 
-var inter = setInterval(function() {
-  if (typeof jxcore == 'undefined')
-    return;
-
-  clearInterval(inter);
-  
-  document.getElementById('clrbtn').onclick = function() {
-    ctrl_log().innerHTML = "";
-  }
-
-  var addIp = function(addr) {
-    document.getElementById('ipaddrs').innerHTML += "<li>" + addr + "</li>";
-  }
-  
-  jxcore.isReady(function() {
-    log('READY');
-    // register log method from UI to jxcore instance
-    jxcore('log').register(log);
-    jxcore('addIp').register(addIp);
-
-    jxcore('app.js').loadMainFile(function(ret, err) {
-      if (err) {
-        alert(JSON.stringify(err));
-      } else {
-        log('Loaded');
-        jxcore_ready();
-      }
-    });
-  });
-}, 5);
-
-function jxcore_ready() {
-  // calling a method from JXcore (app.js)
-  jxcore('asyncPing').call('Hello', function(ret, err) {
-    // register getTime method from jxcore (app.js)
-    var getBuffer = jxcore("getBuffer");
-
-    getBuffer.call(function(bf, err) {
-      var arr = new Uint8Array(bf);
-      log("Buffer size:" + arr.length + " - first item: " + arr[0]);
-    });
-  });
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
 }
